a9f3a27ca65d02b34605247e81afc058af53b776
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-02-23 00:25:42 +0100

    added dotenv + replaced

--- server/index.js
+++ server/index.js
@@ -3,7 +3,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
-import "dotenv/config"
+require('dotenv').config();
+
+
+const configuration = new Configuration({
+    organization: "org-vZ5plwtUxRSAWgx4eUDma7cE",
+    apiKey:process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(configuration);
 import routes from "./src/routes/index.js";
 
 const app =  express()
