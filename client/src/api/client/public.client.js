a6f3a6fee3dd627e3cda3b83f711aaf7ebfbacd1
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-03-08 12:20:08 +0100

    fix problem with public client

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -10,7 +10,7 @@ const publicClient = axios.create({
   },
 });
 
-publicClient.interceptors.use(async (config) => {
+publicClient.interceptors.request.use(async (config) => {
   return {
     ...config,
     headers: {
