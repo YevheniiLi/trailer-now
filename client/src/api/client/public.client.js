c61f76b49b6022f10e0b4ddf56b5fb783a2755ba
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-03-22 16:17:05 +0100

    update api

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://trailer-now-api.vercel.app/api/v1/";
+const baseURL = "http://192.168.1.46:3000/api/v1/";
 
 
 const publicClient = axios.create({
