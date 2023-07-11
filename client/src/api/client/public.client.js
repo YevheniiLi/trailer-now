9e6f06448f1277c8a591b220d9bcb8c6af3224f4
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-07-11 23:44:56 +0200

    update api with vercel

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://trailer-now.vercel.app/api/v1/";
 
 
 const publicClient = axios.create({
